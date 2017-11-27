3b18ae685e99c07e680b5d77e4a0404d5dc43c5f
yashkapoor007 <ykapoor_be15@thapar.edu>
2017-11-27 15:20:28 +0530

    delete at beginning

---
+++ A3P1e_101515063.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#include "Node.cpp"
+using namespace std;
+Node* deleteAtbeginning(Node* head)
+{
+  if(head!=NULL)
+  {
+      Node* temp=head;
+      head=head->next;
+      delete temp;
+  }
+  return head;
+}
+Node* takeInput()
+{
+  Node* head=NULL;
+  Node* tail=NULL;
+  int data;
+  cin>>data;
+  while(data!=-1)
+  {
+    Node* newNode= new Node(data);
+    if(head==NULL)
+    {
+      head=newNode;
+      tail=newNode;
+    }
+    else
+    {
+      tail->next=newNode;
+      tail=tail->next;
+    }
+    cin>>data;
+  }
+  return head;
+}
+void print(Node* head)
+{
+  Node* temp=head;
+  while(temp!=NULL)
+  {
+    if(temp->next!=NULL)
+    cout<<temp->data<<"->";
+    else
+    cout<<temp->data;
+    temp=temp->next;
+  }
+  cout<<endl;
+}
+int main()
+{
+  cout<<"enter the elements"<<endl;
+  Node* head=takeInput();
+  head=deleteAtbeginning(head);
+  head=deleteAtbeginning(head);
+  print(head);
+}
